48d5c52f0c54bf0288234f882ebf5f048703c9eb
DiegoMartinSa <diegomartinsa02@gmail.com>
2022-09-20 16:01:38 -0300

    Primera Subida

---
+++ Ejemplo1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Ejemplo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+            int v;
+
+        }
+    }
+}
